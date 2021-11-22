693ae0bf3c4b00b39a5431c15a2e65c385135ec7
KuroeGame <cloe-13500@hotmail.fr>
2021-11-22 17:38:27 +0100

    exercice voiture

---
+++ Objet voiture/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let myCar = {
+    make: 'Renault',
+    model: 'Twingo 3',
+    year: 2014,
+    buy: new Date("2015-07-31"),
+    passagers :[ 'Audrey', 'Gribouille' ]
+};
+
+
+document.querySelector("#content");
+const DIV = document.querySelector("#content");
+let content = DIV.innerHTML;
+DIV.innerHTML = `<h2>Informations sur la voiture :</h2>
+                <ul>
+                    <li>Marque : ${myCar.make} ${myCar.model}</li>
+                    <li>Année de fabrication : ${myCar.year}</li>
+                    <li>Date de l'achat : ${myCar.buy.toDateString()}</li>
+                    <li>Passagers : 
+                        <ol>
+                            <li>${myCar.passagers[0]}</li>
+                            <li>${myCar.passagers[1]}</li>
+                        </ol>
+                    </li>
+                </ul>`;
